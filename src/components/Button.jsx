83721a76af2b19e9372dd3abd8bcc996dfa43fa6
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-08-12 16:12:15 -0300

    feature função clicar no botão das instituições para as respectivas habilidades ficarem ativa, obs: ainda falta terminar

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable react/prop-types */
 import styles from "./Button.module.css";
 
-function Button({ children, className }) {
-  return <button className={` ${styles.button}`}>{children}</button>;
+function Button({ children, onClick }) {
+  return (
+    <button onClick={onClick} className={` ${styles.button}`}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,19 +1,47 @@
+import { useState } from "react";
 import Button from "./Button";
 import styles from "./Skills.module.css";
 import TitleContainer from "./TitleContainer";
 
 function Skills() {
+  const [addedClass, setAddedClass] = useState(false);
   const institutions = ["Origamid", "Cubos Academy", "DIO", "Rocketseat"];
-  const skills = [
-    "React.js",
-    "Node.js",
-    "CSS  Flexbox",
-    "SQL",
-    "JavaScript",
-    "Git",
-    "MUI",
-    "HTML",
-  ];
+  const [skills, setSkills] = useState([
+    {
+      name: "React.js",
+      status: false,
+      institutions: "Origamid, Cubos Academy",
+    },
+    { name: "CSS  Flexbox", status: false, institutions: "Origamid" },
+    { name: "HTML", status: false, institutions: "" },
+    { name: "Git", status: false, institutions: "" },
+    { name: "MUI", status: false, institutions: "" },
+    { name: "HTML", status: false, institutions: "" },
+    { name: "JavaScript", status: false, institutions: "" },
+  ]);
+
+  function handleClickInstitutions({ target }) {
+    if (target.classList.contains("buttonActive")) {
+      target.classList.remove("buttonActive");
+      setAddedClass(false);
+    }
+    if (!addedClass) {
+      target.classList.add("buttonActive");
+      setSkills((state) => {
+        return state.map((value) => {
+          if (value.institutions.includes(target.innerText)) {
+            return {
+              ...value,
+              status: true,
+            };
+          }
+          return value;
+        });
+      });
+
+      setAddedClass(true);
+    }
+  }
 
   return (
     <section className={`container ${styles.skills}`}>
@@ -22,15 +50,22 @@ function Skills() {
       <div className={styles.containerGroup}>
         <div className={styles.groupButton}>
           {institutions.map((instit, index) => (
-            <Button className="buttonActive" key={index}>
+            <Button
+              onClick={(e) => handleClickInstitutions(e)}
+              className="buttonActive"
+              key={index}
+            >
               {instit}
             </Button>
           ))}
         </div>
         <div className={styles.groupSkill}>
           {skills.map((skill, index) => (
-            <div key={index} className={`${styles.card}`}>
-              {skill}
+            <div
+              key={index}
+              className={`${skill.status && "cardActive"} ${styles.card}`}
+            >
+              {skill.name}
             </div>
           ))}
         </div>
